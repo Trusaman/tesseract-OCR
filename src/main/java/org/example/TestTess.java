6598dd4aae91ab7c8705690e47e4184814814ad6
KETOAN03 <ngocanha85@gmail.com>
2023-06-02 18:06:39 +0700

    offline OCR/fixed-read-image

--- src/main/java/org/example/TestTess.java
+++ src/main/java/org/example/TestTess.java
@@ -18,13 +18,13 @@ public class TestTess {
         File file = new File(currentPath);
         File[] files = file.listFiles();
         for (File f : files) {
-            if (f.getName().equals("img.png")) {
+            if (f.getName().equals("img.jpg")) {
                 f.delete();
             }
         }
 
         // get image from this web
-        String encodedImage = "data:image/jpg;base64, /9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAoAF8DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD1SisfxRNLb+HLuWGV4pF2YdGKkfOvcVUs/E9pBZafHci5PmRIrXLRkoX2jPzHqc9aAOjormNT1S/HiA2AvotMthHvWeSMMJDgcfNx3P5Va8Nale6hHdC6KypDJsjuFXaJRzz+g/OgDdorB8Jajd6npUs15L5sizlAdoXjap7Aepo0HUbu91XWYbiXfHbz7IhtA2jc47DnoOtAG9RXGa/4i1Bd8+mSiOzhlEBk2hvMcgk4yDwMfrXU6jfw6ZYyXU3IXhVHVm7AUAWqK5bQtT1afxHc2WoyjCQeb5QQDYTtIGcZ4DYqK81S+bxTeWA1mLT7aJFZGkjQjO1ePmx6k9e1AHXUVQ0hna0YyanFqJ3n97GqqBwPl+U49/xq/QBj+KIZbjw5dxQxPLI2zCIpYn517CsfXrK5l8G6XBDazPMnlbo0jJZcRnOQOnNdhRQByep200Xihr2806bULFotsSRx+ZsOB/D9Qfzq14UtLu2S9eaF7e3ll3QQOeUHPbt1A/CuiooA5HRnu/DUdzYz6ZeXCGUvHLbR7wwwBz6dBVjQ9KvDb6vPcK1rJqLMVXq0ed3P/j36V01FAHC65oGp2uiwW0V097CkoCwRWgBXhvmyMk/j61vXuh3t7HYl9V/0i1Zm8026kOSeDtzgYHFblFAHHWGmatH4yuJZLqUgIpe5NsAsy/JlB2Hpkc8VZi0z7R441CW6sfNtWgXY8sW5C2E6EjGev611FFAEVvbW9rGY7eCKFCclY0Cgn14qWiigAooooAKKKKACiiigAooooAKKKKACiiigD//Z";
+        String encodedImage = "data:image/jpg;base64, /9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAoAF8DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD1SisvxHeT2Gg3NzbPsmTbtbAOMsAeD7GsrQNTlvrq38zxAs7lNz2htQhzt6Bu+D6elAHU0VzuoaxNZeLrK0kukhsXgLyB9oGfnx8x6cgd6Zq2vFdV0iHTr6F45p9k4jZXyNygZ646mgDpaK5+LUru38XSaddzb7edN9vlQMd8ZA9mHPoKJdSu7nxdFp1pNst4E33GFBz3xkj3UcepoA6CiuX0PUWTUvED3l2/kQT/AC+Y5KoNz8DPToOBWk3iG0j04X08c8MLNtjEiDdJ7qAc4+uKANaismz8QWt3dravDc2s7jKJcxbN49qxoPE92fEVzG9pfvaiIFLVbceYh+Xkjrjr37igDr6KKKAKepTtb2wdbJ7xd43RoASB1zg9eQK5+dLnXPEGn3MOnXFpFatukluE2MwyDjHfp+prrKKAOY1LT3uvG9hJJaNNaC3Idmj3Rg/PgHjHXH6UzW9JSPWdEksNPVUW4zK0EOAAGTBbA+vX3rqqKAOc8V2NxIlpqFlE8l1ayghUUklfoOvIH5mjwpY3EaXeoXsTx3V1KSVdSCF+h6ck/kK6OigDhI9DvrnUdXuEidHju/OhjmQiOf5n454PbB9/erutQXmuWFjeQ2lzDNbSEywEFH5xypI56cH3rrqKAOLtrMXOpWcht/EEjwyqwa8dQicjJyRyOOg61Zl+06X4yu9Qawu7i3nhCKbePfzhev8A3ya6uigAooooAKKKKACiiigAooooAKKKKACiiigAooooA//Z";
 
         // Remove any whitespace characters from the encoded image data string
         encodedImage = encodedImage.replaceAll("\\s+", "");
@@ -36,7 +36,7 @@ public class TestTess {
         byte[] imageData = Base64.getDecoder().decode(encodedImageData);
 
         // Save the image data to a file
-        try (FileOutputStream fos = new FileOutputStream(new File("output.jpg"))) {
+        try (FileOutputStream fos = new FileOutputStream(new File("img.jpg"))) {
             fos.write(imageData);
             System.out.println("Image saved successfully.");
         } catch (IOException e) {
@@ -51,7 +51,7 @@ public class TestTess {
             // the path of your tess data folder
             // inside the extracted file
             String text
-                    = tesseract.doOCR(new File("img_1.png"));
+                    = tesseract.doOCR(new File("img.jpg"));
 
             // path of your image file
             System.out.print(text);
